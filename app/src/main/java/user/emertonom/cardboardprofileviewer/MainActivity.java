263c72266a4ebfffeb4a64be256b7fabaf3f1bc3
emertonom <emertonom@yahoo.com>
2016-11-25 16:35:36 -0800

    Android Studio auto-updated various files for compatibility with the latest version of studio
    Added support for Marshmellow/Nougat runtime permission requests
    
    Added display of FOV measurements

--- app/src/main/java/user/emertonom/cardboardprofileviewer/MainActivity.java
+++ app/src/main/java/user/emertonom/cardboardprofileviewer/MainActivity.java
@@ -1,26 +1,24 @@
 package user.emertonom.cardboardprofileviewer;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
+import android.view.View;
 import android.view.MenuItem;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
-
-import org.w3c.dom.Text;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -29,6 +27,7 @@ import java.io.IOException;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_WRITE_PERMISSON = 17;
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -39,6 +38,53 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_WRITE_PERMISSON);
+        } else readParams();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        Log.d("emertonom", "Received result of permission request");
+        switch (requestCode) {
+            case REQUEST_WRITE_PERMISSON: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay!
+                    readParams();
+                } else {
+                    // permission denied, boo!
+                    DialogInterface.OnClickListener listener = new AlertDialog.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    };
+                    AlertDialog.Builder dlg = new AlertDialog.Builder(this);
+                    dlg.setTitle("Error");
+                    dlg.setMessage("Unable to locate Cardboard Device Parameters");
+                    dlg.setCancelable(false);
+                    dlg.setPositiveButton("Exit", listener);
+                    dlg.create();
+                    dlg.show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    public void readParams() {
         FileInputStream fstream = null;
         String extDir  = Environment.getExternalStorageDirectory().getPath();
         if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState()) ) {
@@ -137,6 +183,27 @@ public class MainActivity extends AppCompatActivity {
                 interlensText.setText(interlensFloat * 1000 + "mm");
             }
 
+            if (deviceParams.getLeftEyeFieldOfViewAnglesCount() > 0) {
+                TextView FOVLeftView = (TextView) findViewById(R.id.FOVLeft);
+                float FOVLeftFloat = deviceParams.getLeftEyeFieldOfViewAngles(0);
+                FOVLeftView.setText(FOVLeftFloat + " degrees");
+            }
+            if (deviceParams.getLeftEyeFieldOfViewAnglesCount() > 1) {
+                TextView FOVRightView = (TextView) findViewById(R.id.FOVRight);
+                float FOVRightFloat = deviceParams.getLeftEyeFieldOfViewAngles(1);
+                FOVRightView.setText(FOVRightFloat + " degrees");
+            }
+            if (deviceParams.getLeftEyeFieldOfViewAnglesCount() > 2) {
+                TextView FOVBottomView = (TextView) findViewById(R.id.FOVBottom);
+                float FOVBottomFloat = deviceParams.getLeftEyeFieldOfViewAngles(2);
+                FOVBottomView.setText(FOVBottomFloat + " degrees");
+            }
+            if (deviceParams.getLeftEyeFieldOfViewAnglesCount() > 3) {
+                TextView FOVTopView = (TextView) findViewById(R.id.FOVTop);
+                float FOVTopFloat = deviceParams.getLeftEyeFieldOfViewAngles(3);
+                FOVTopView.setText(FOVTopFloat + " degrees");
+            }
+
             // verticalAlignmentText
             if (deviceParams.hasVerticalAlignment()) {
                 TextView verticalAlignmentText = (TextView) findViewById(R.id.verticalAlignmnentText);
@@ -215,11 +282,9 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+        // Force redraw
+        View rootView= (View) findViewById(R.id.rootView);
+        rootView.invalidate();
     }
 
     @Override
